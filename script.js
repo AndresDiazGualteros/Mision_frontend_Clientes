c00f25872acbf7009b57e488790bcb2fe2aa712a
AndresDiazGualteros <luisandresdiazgualteros22@gmail.com>
2023-01-25 12:11:27 -0500

    final

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 const crearClientes = document.getElementById('CrearCliente');
 const modal = document.getElementById('Modal');
 const atras = document.getElementById('atras');
-/* formulario constantes */
-const nombre = document.getElementById('Nombre');
-const nitCC = document.getElementById('Nit/CC');
-const contacto = document.getElementById('contacto');
-const apellidos = document.getElementById('apellidos');
-const Direccion = document.getElementById('Direccion');
-const telefono = document.getElementById('telefono');
-const CupoTotal = document.getElementById('cupo_total');
-const CupoDisponible = document.getElementById('Cupo_disponible');
-const diaGracia = document.getElementById('Dias_de_gracias');
+const tabla = document.getElementById('tabla');
+const GuardarCambios = document.getElementById('GuardarCambios');
  
 
 
@@ -26,17 +18,39 @@ function Atras() {
 }
 
 
-class Personas{
-    constructor(nombre, nitCC, contacto,apellidos, Direccion, telefono, CupoTotal, CupoDisponible, diaGracia){
-    this.nombre = nombre
-    this.nitCC=nitCC
-    this.contacto=contacto
-    this.apellidos=apellidos
-    this.Direccion=Direccion
-    this.telefono=telefono
-    this.CupoTotal=CupoTotal
-    this.CupoDisponible=CupoDisponible
-    this.diaGracia=diaGracia
-    }
-}
+GuardarCambios.addEventListener('click', ()=>{
+    /* formulario constantes */
+    const nombre = document.getElementById('Nombre').value;
+    const nitCC = document.getElementById('Nit/CC').value;
+    const contacto = document.getElementById('contacto').value;
+    const apellidos = document.getElementById('apellidos').value;
+    const Direccion = document.getElementById('Direccion').value;
+    const telefono = document.getElementById('telefono').value;
+    const CupoTotal = document.getElementById('cupo_total').value;
+    const CupoDisponible = document.getElementById('Cupo_disponible').value;
+    const diaGracia = document.getElementById('Dias_de_gracias').value;
+    
 
+
+    if(nombre!='' && apellidos!='' && Direccion!='' && nitCC!='' && contacto!='' && telefono!='' && CupoTotal!='' && CupoDisponible!='' && diaGracia!='' && GuardarCambios!=''){
+        tabla.innerHTML+=`
+        <tr>
+        <td>${nombre}</td>
+        <td>${apellidos}</td>
+        <td>${nitCC}</td>
+        <td>${Direccion}</td>
+        <td>${ciudad}</td>
+        <td>${telefono}</td>
+        <td>${CupoDisponible}}</td>
+        <td>Activo</td>
+        <td>
+            <span class="material-symbols-outlined">person_add</span>
+            <span class="material-symbols-outlined">delete</span>
+        </td>
+    </tr>
+        `
+        modal.style.top = '-1200px';
+    }else{
+        alert('Campos vacios')
+    }
+})
